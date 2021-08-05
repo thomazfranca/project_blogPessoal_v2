d2734c3ff77a8eec406a4d6a84ae3883ed99eb5e
thomazfranca <trsf1990@gmail.com>
2021-08-05 08:32:33 -0300

    consumo de ambiente

--- src/app/model/userLogin.ts
+++ src/app/model/userLogin.ts
@@ -1,7 +1,7 @@
 export class UserLogin {
   public id: number
   public nome: string
-  public nomeUsuario: string
+  public usuario: string
   public senha: string
   public token: string
   public foto: string
